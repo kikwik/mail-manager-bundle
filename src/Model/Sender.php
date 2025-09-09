3ddf5e5238f6a831f5bc479200b3d15bbff02baa
Klaus <support@kikwik.it>
2025-09-10 01:59:51 +0200

    fix relation between entities

--- src/Model/Sender.php
+++ src/Model/Sender.php
@@ -4,7 +4,39 @@ namespace Kikwik\MailManagerBundle\Model;
 
 abstract class Sender
 {
-    public string $name;
+    protected ?int $id = null;
 
-    public string $email;
+    protected ?string $name = null;
+
+    protected ?string $email =  null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+
 }
--- src/Model/Template.php
+++ src/Model/Template.php
@@ -4,13 +4,78 @@ namespace Kikwik\MailManagerBundle\Model;
 
 abstract class Template
 {
-    public string $code;
+    protected ?int $id = null;
 
-    public string $event;
+    protected ?string $code = null;
 
-    public Sender $sender;
+    protected ?string $event = null;
 
-    public string $subject;
+    protected ?Sender $sender = null;
 
-    public string $body;
+    protected ?string $subject = null;
+
+    protected ?string $body = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): static
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function getEvent(): ?string
+    {
+        return $this->event;
+    }
+
+    public function setEvent(?string $event): static
+    {
+        $this->event = $event;
+        return $this;
+    }
+
+    public function getSender(): ?Sender
+    {
+        return $this->sender;
+    }
+
+    public function setSender(?Sender $sender): static
+    {
+        $this->sender = $sender;
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): static
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(?string $body): static
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+
 }
